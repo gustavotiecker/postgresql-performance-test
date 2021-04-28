a02561c304925c3195e179cc6abcf4a15207b145
JoviMazzochin <joaomazzochin@gmail.com>
2021-04-28 19:34:57 -0300

    Implementing Timer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,32 @@ import java.util.UUID;
 public class Main {
 
     private static String sqlInsert, sqlSelect, sqlUpdate, sqlDelete;
+    private static long count = 0;
+    private static double timeSelect = 0, timeInsert = 0, timeUpdate = 0, timeDelete = 0;
 
     public static void main(String[] args) throws SQLException {
         selectOrderItem();
+        insertOrderItem();
+        updateOrderItem();
+        deleteOrderItem((int) Math.random() * 1000000);
+
+        //obtendo medias
+        timeSelect = timeSelect / count;
+        timeInsert = timeInsert / count;
+        timeUpdate = timeUpdate / count;
+        timeDelete = timeDelete / count;
+
+        System.out.println("=============== Averages ===============");
+        System.out.println("Select: " + timeSelect +
+                "Insert: " + timeInsert +
+                "Update: " + timeUpdate +
+                "Delete: " + timeDelete);
+        System.out.println("========================================");
     }
 
     private static void insertOrderItem() {
+        //for
+        count = 0;
         OrderItem orderItem = createOrderItem();
         sqlInsert = "INSERT INTO order_items VALUES ('" + orderItem.getOrderId() +
                 "', " + orderItem.getOrderItemId() +
@@ -18,25 +38,41 @@ public class Main {
                 "', '" + orderItem.getShippingLimitDate() +
                 "', " + orderItem.getPrice() +
                 ", " + orderItem.getFreightValue() + ");";
+                accessDatabase(sqlInsert);
+                count ++;
+        //endfor
 
-        accessDatabase(sqlInsert);
     }
 
     private static void selectOrderItem() throws SQLException {
+        //for
+        count = 0;
         sqlSelect = "SELECT product_id, seller_id, shipping_limit_date, price FROM order_items AS oi WHERE oi.price > " + Math.random() * 51 + ";";
         accessDatabase(sqlSelect);
+        //endfor
+
+
     }
 
     private static void updateOrderItem() {
+        //for
+        count = 0;
         sqlUpdate = "UPDATE order_items SET freight_value = freight_value + (freight_value * 0.2) WHERE price > " + Math.random() * 101 + ";";
         accessDatabase(sqlUpdate);
+        //endfor
+
+
     }
 
     // TO DO
     private static void deleteOrderItem(int num) {
+        //for
+        count = 0;
         sqlDelete = "DELETE FROM order_items WHERE price > 300 AND freight_value > 5  AND freight_value < 5 + 5;";
-
         accessDatabase(sqlDelete);
+        //endfor
+
+
     }
 
     private static void accessDatabase(String query) {
@@ -51,8 +87,21 @@ public class Main {
             }
 
             //Begin thread - Barbada, 2 minutinhos
+            long startTime = System.nanoTime();
             ResultSet resultSet = preparedStatement.executeQuery();
+            long endTime = System.nanoTime();
+
+            long executionTime = endTime - startTime;
             //End Thread - Feito carreta
+            if(query.startsWith("SELECT")) {
+                timeSelect += executionTime;
+            } else if(query.startsWith("INSERT")) {
+                timeInsert += executionTime;
+            } else if(query.startsWith("UPDATE")) {
+                timeUpdate += executionTime;
+            } else if(query.startsWith("DELETE")) {
+                timeDelete += executionTime;
+            }
 
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
